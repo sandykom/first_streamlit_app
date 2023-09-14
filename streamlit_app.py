3bedcebb2935f4d22f73ab39a4dc6545284fd1bf
sandykom <144969001+sandykom@users.noreply.github.com>
2023-09-14 21:09:05 +1000

    Update streamlit_app.py
    display response as Json

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,6 @@ st.header("Fruityvice Fruit Advice!")
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
 
