142babc709b2fe513fbc97225a29d67d0f049fae
sandykom <144969001+sandykom@users.noreply.github.com>
2023-09-14 22:48:42 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,17 +24,29 @@ st.dataframe(fruits_to_show)
 
 st.header("Fruityvice Fruit Advice!")
 
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = st.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    st.error("Please select a fruit to get information")
+ else:
+ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+ st.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-# st.text(fruityvice_response.json())
+except URLError as e:
+st.Error()
+ 
+    
+# st.write('The user entered ', fruit_choice)
 
 
-# normalize the json output 
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# display the normalised dataframe
-st.dataframe(fruityvice_normalized)
+# # st.text(fruityvice_response.json())
+
+
+# # normalize the json output 
+# fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+# # display the normalised dataframe
+
 
 
 st.stop()
