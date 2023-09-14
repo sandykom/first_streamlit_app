13b4cfc2b70b4a6c4df3b6a11875bd9b67c3bea6
sandykom <144969001+sandykom@users.noreply.github.com>
2023-09-14 11:06:56 +1000

    Update streamlit_app.py
    corrected the alias syntax

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit st
+import streamlit as st
 st.title('I am an early person 🐔')
 
 st.header('learning to add a ine below the title')
@@ -6,6 +6,6 @@ st.text('adding first line below header')
 st.text('adding second line')
 
 
-import pandas pd
+import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 st.dataframe(my_fruit_list)
