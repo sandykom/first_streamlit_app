c1298f5bed48bf2d132dad3946347884726008e6
sandykom <144969001+sandykom@users.noreply.github.com>
2023-09-14 22:43:56 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import pandas as pd
+import requests
+import snowflake.connector
+from urllib,.error import URLError
+
+
 st.title('I am an early person 🐔')
 
 st.header('learning to add a ine below the title')
@@ -6,7 +12,6 @@ st.text('adding first line below header')
 st.text('adding second line')
 
 
-import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -22,7 +27,6 @@ st.header("Fruityvice Fruit Advice!")
 fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
 st.write('The user entered ', fruit_choice)
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 # st.text(fruityvice_response.json())
 
@@ -33,7 +37,9 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 st.dataframe(fruityvice_normalized)
 
 
-import snowflake.connector
+st.stop()
+
+
 
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
