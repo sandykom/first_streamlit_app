2922f8ebd2272a188d88e988b140d5c8c839d4b8
sandykom <144969001+sandykom@users.noreply.github.com>
2023-09-14 10:59:43 +1000

    Update streamlit_app.py
    Added text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit
 streamlit.title('I am an early person')
 
 streamlit.header('learning to add a ine below the title')
+streamlit.text('adding first line below header')
+streamlit.text('adding second line')
