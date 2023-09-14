fd0300cabeb38bb4906059ed844eb980be81a787
sandykom <144969001+sandykom@users.noreply.github.com>
2023-09-14 21:14:32 +1000

    Update streamlit_app.py
    prompt asking user to choose the fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,11 @@ st.dataframe(fruits_to_show)
 
 st.header("Fruityvice Fruit Advice!")
 
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 # st.text(fruityvice_response.json())
 
 
